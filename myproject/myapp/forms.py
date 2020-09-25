cb805128ab295da850d8bbb8a40c1747cc597afe
Phat <tlphat18@apcs.vn>
2020-09-25 13:24:07 +0700

    First commit

---
+++ myproject/myapp/forms.py
@@ -0,0 +1,8 @@
+from django import forms 
+from myapp.models import ImageAndroid
+
+class AndroidForm(forms.ModelForm):
+
+    class Meta:
+        model = ImageAndroid
+        fields = ['image']
---
+++ myproject/myapp/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from myapp.forms import AndroidForm
+from django.views.decorators.csrf import csrf_exempt
+import base64
+
+# Create your views here.
+# def home(request):
+#     return HttpResponse("Hello World")
+
+# def add(request):
+#     if request.method == 'GET':
+#         a = float(request.GET['a'])
+#         b = float(request.GET['b'])
+#         c = a + b 
+#         return HttpResponse(str(c))
+#     else:
+#         return HttpResponse('method not supported')
+
+@csrf_exempt
+def android_image_view(request):
+    if request.method == 'POST':
+        form = AndroidForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('successfully uploaded')
+        return HttpResponse('form error')
+    else:
+        return HttpResponse('method not supported')
+
+def retrieve_image(request):
+    if request.method == 'GET':
+        name = 'media/images/' + str(request.GET['name'])
+        image_data = ''
+        with open(name, "rb") as image_file:
+            image_data = base64.b64encode(image_file.read()).decode('utf-8')
+        image_file.close()
+        return HttpResponse(image_data)
+    else:
+        return HttpResponse('method not supported')
+
+def success(request): 
+    return HttpResponse('successfully uploaded')
